45c9703b451e7e2ca81b6af11142acc981a25ba8
suada <suadaljuaid1@gmail.com>
2025-12-27 22:31:17 +0300

    lab7 -CircularLinkedList finished

--- src/CirculyLinkedList/CircularLinkedList.java
+++ src/CirculyLinkedList/CircularLinkedList.java
@@ -24,7 +24,6 @@ public void addFirst(E data){
     else {
         Node<E> newNode=new Node(data,tail.getNext());
         tail.setNext(newNode);
-
     }
     size++;
 }
@@ -32,6 +31,66 @@ public void addLast(E data) {
     addFirst(data);
     tail=tail.getNext();
 }
+public E getFirst(){
+    return tail.getNext().getData();
+}
+public E getLast(){
+        return tail.getData();
+    }
+public E removeFirst() {
+    if (isEmpty()) return null;
+    E delete=tail.getNext().getData();
+    if(tail==tail.getNext()){
+        tail=null;
+    }
+    else {
+        tail.setNext(tail.getNext().getNext());
+    }
+    size--;
+    return delete;
+}
+public E removeLast(){
+    if (isEmpty())return null;
+    E delete=tail.getData();
+    if(tail==tail.getNext()){
+        tail=null;
+    }
+    else {
+        Node<E>temp=tail.getNext();
+        while (temp.getNext()!=tail){
+            temp=temp.getNext();
+        }temp.setNext(tail.getNext());
+        tail=temp;
+    }
+    size--;
+    return delete;
+}
+public void display(){
+    if (isEmpty()){
+        System.out.println("List is empty");
+        return;}
+    Node<E>temp=tail.getNext();
+    do {
+        System.out.print(temp.getData()+"---->");
+        temp=temp.getNext();
+    }while (temp!=tail.getNext());
+    System.out.println("go first ("+temp.getData()+")");
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
 
 
 
--- src/CirculyLinkedList/Main.java
+++ src/CirculyLinkedList/Main.java
@@ -1,4 +1,19 @@
 package CirculyLinkedList;
 
 public class Main {
+    public static void main(String[] args) {
+        CircularLinkedList<String> clist=new CircularLinkedList<>();
+        clist.addFirst("Gamdan");
+        clist.addFirst("Ali");
+        clist.addFirst("Emad");
+        clist.addLast("Iss ALdain");
+        clist.addLast("Abdu");
+        clist.removeFirst();
+        clist.removeLast();
+        clist.display();
+
+
+
+
+    }
 }
